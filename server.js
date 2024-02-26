758f13fdf2801ffb5adfe6672891049c380af267
330namajnoon <sina.cappac@gmail.com>
2024-02-26 13:59:12 +0100

    public

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { Server } = require("sm-express-server");
 
-const server = new Server(4001, "/public/");
+const server = new Server(4001, "/public");
 
 server.start(() => {
     console.log("server is up on port 4001!");
